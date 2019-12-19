3364e1205101605644f7d21367809d50b37df10d
Paweł Banaś <paweloot@gmail.com>
2019-12-19 19:35:03 +0100

    Initial commit

---
+++ src/main/java/com/paweloot/gotrest/GotrestApplication.java
@@ -0,0 +1,13 @@
+package com.paweloot.gotrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paweloot/gotrest/GotrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paweloot.gotrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
