fc93866a745e708eea9d885443eaf73225e57076
Paweł Banaś <paweloot@gmail.com>
2019-12-19 23:33:05 +0100

    Create MtnRangeRepository using Spring Data JPA

---
+++ src/main/java/com/paweloot/gotrest/repository/MtnRangeRepository.java
@@ -0,0 +1,8 @@
+package com.paweloot.gotrest.repository;
+
+import com.paweloot.gotrest.entity.MtnRange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MtnRangeRepository extends JpaRepository<MtnRange, Integer> {
+
+}
